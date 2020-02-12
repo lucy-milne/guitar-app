fd18d6143ed093ae804748fafa0824857b9f9cdc
Lucy Milne <lucy.milne@xero.com>
2020-02-13 10:56:23 +1300

    initial setup of routing

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class QuizList extends Component {
+    render () {
+        return (
+            <div> Hi </div>
+        )
+    }
+}
+
+export default QuizList;
