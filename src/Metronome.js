149b089da1a655f5440e0c67393991bd96a0427e
Lucy Milne <lucy.milne@xero.com>
2020-02-13 16:28:20 +1300

    completed metronome styling

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { IoIosPlay, IoIosPause, IoIosRemove, IoIosAdd } from 'react-icons/io';
 import 'startbootstrap-sb-admin-2/css/sb-admin-2.css';
+import './Metronome.css';
 
 class Metronome extends Component {
     constructor (props) {
@@ -16,10 +17,10 @@ class Metronome extends Component {
 
     render () {
         return (
-            <card className='card' style={{marginLeft: '30%', marginTop: '5%', padding: '2%', width: '35%'}}> 
-                <div style={{display: 'flex', flexDirection: 'row'}}>
+            <div className='card shadow' style={{marginLeft: '30%', marginTop: '10%', padding: '3%', width: '35%'}}> 
+                <div style={{display: 'flex', flexDirection: 'row', marginLeft: '25px', marginRight: '40px'}}>
                     <div className='h1 mb-0 font-weight-bold text-gray-800' style={{flexDirection: "row-start"}}> 70</div>
-                    <div className='h6 mb-0 font-weight-bold text-gray-800' style={{marginTop: '7%'}}> &nbsp; bpm </div>
+                    <div className='h6 mb-0 font-weight-bold text-gray-800' style={{marginTop: '2%'}}> &nbsp; bpm </div>
                         
                     <button className='btn btn-success btn-circle btn-lg' style={{flexDirection: "row-end", marginLeft: '60%'}} onClick={() => this.buttonClicked()}>
                         {this.state.play ? <IoIosPause /> : <IoIosPlay />}
@@ -32,12 +33,12 @@ class Metronome extends Component {
 
                 <div style={{display: 'flex', flexDirection: 'row'}}>
                     <IoIosRemove size={60} color={'#1cc88a'} />
+                    <input className='yay slider' type="range" min="60" max="240" value={220} />
                     <IoIosAdd size={60} color={'#1cc88a'} />
                 </div>
 
 
-
-            </card>
+            </div>
         )
     }
 }
