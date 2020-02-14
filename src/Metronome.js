df5bd3d2bef366e807df40d9c4d17c4d14280b6e
Lucy Milne <lucy.milne@xero.com>
2020-02-14 13:24:04 +1300

    leaving work push

--- src/Metronome.js
+++ src/Metronome.js
@@ -7,9 +7,14 @@ class Metronome extends Component {
     constructor (props) {
         super(props);
         this.state = {
+            bpm: 100,
             play: false
         }
     }
+
+    changeBpm(event) {
+        this.setState({bpm: event.target.value})
+    } 
 
     buttonClicked() {
         this.setState({play: !this.state.play})
@@ -19,10 +24,10 @@ class Metronome extends Component {
         return (
             <div className='card shadow' style={{marginLeft: '30%', marginTop: '10%', padding: '3%', width: '35%'}}> 
                 <div style={{display: 'flex', flexDirection: 'row', marginLeft: '25px', marginRight: '40px'}}>
-                    <div className='h1 mb-0 font-weight-bold text-gray-800' style={{flexDirection: "row-start"}}> 70</div>
-                    <div className='h6 mb-0 font-weight-bold text-gray-800' style={{marginTop: '2%'}}> &nbsp; bpm </div>
+                    <div className='h1 mb-0 font-weight-bold text-gray-800 col-xl-4' style={{flexDirection: "row-start"}}> {this.state.bpm} </div>
+                    <div className='h6 mb-0 font-weight-bold text-gray-800' style={{marginTop: '25px', marginLeft: '-25px'}}> &nbsp; bpm </div>
                         
-                    <button className='btn btn-success btn-circle btn-lg' style={{flexDirection: "row-end", marginLeft: '60%'}} onClick={() => this.buttonClicked()}>
+                    <button className='btn btn-success btn-circle btn-lg' style={{flexDirection: "row-end", marginLeft: '115px'}} onClick={() => this.buttonClicked()}>
                         {this.state.play ? <IoIosPause /> : <IoIosPlay />}
                     </button>
                 </div>
@@ -33,7 +38,7 @@ class Metronome extends Component {
 
                 <div style={{display: 'flex', flexDirection: 'row'}}>
                     <IoIosRemove size={60} color={'#1cc88a'} />
-                    <input className='yay slider' type="range" min="60" max="240" value={220} />
+                    <input className='yay slider' type="range" min="60" max="240" value={this.state.bpm} onChange={(event) => this.changeBpm(event)}/>
                     <IoIosAdd size={60} color={'#1cc88a'} />
                 </div>
 
